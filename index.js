5ad477399b89246bfa5fa6e48c3b88b46d072c42
ConnorMul <mulhollandconnor@icloud.com>
2021-03-02 19:34:23 -0500

    removed console.logs

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const fetch = require("node-fetch");
 
 async function fetchAPIData() {
-     const response = await fetch("https://storage.googleapis.com/airgarage/airgarage-data.json")
-    // .then(r => r.json())
-    // .then(data => {
-    //     console.log(data)
-    // })
+    const response = await fetch("https://storage.googleapis.com/airgarage/airgarage-data.json")
+    
     return data = await response.json()
-    // console.log(data)
+   
 
 }
 
@@ -16,7 +13,7 @@ async function executeCommand() {
     if (process.argv[2] === "locate") {
 
     const data = await fetchAPIData()
-    // console.log(process.argv.slice(3))
+   
     const filteredArray = await data.forEach((parkingLot) => {
         if(parkingLot.address.state === process.argv[3]) {
             console.log(parkingLot.name)
@@ -24,7 +21,7 @@ async function executeCommand() {
     })
     } else if (process.argv[2] === "findPriceHourlyLTE") {
         const data = await fetchAPIData()
-        // console.log(parseInt(process.argv.slice(3).join("")))
+        
         const filteredArray = await data.forEach((parkingLot) => {
             if (parkingLot.price_hourly <= process.argv[3]) {
                 console.log(parkingLot.name)
@@ -32,7 +29,7 @@ async function executeCommand() {
         })
     } else if (process.argv[2] === "findPriceHourlyGT") {
         const data = await fetchAPIData()
-        // console.log(parseInt(process.argv.slice(3).join("")))
+        
         const filteredArray = await data.forEach((parkingLot) => {
         if (parkingLot.price_hourly > process.argv[3]) {
             console.log(parkingLot.name)
